55a55392ef474ae1ad93fa8eed43ebabd634d40c
Elephant_Man <grogan.charlie@gmail.com>
2018-12-04 01:10:31 -0600

    string activity

---
+++ strings/strings.js
@@ -0,0 +1,22 @@
+// Strings (or a text string) is a series of character like "Hello there! 911 🕷"
+// As long as everything in contained within quotations ( "" or '' ) single or double it doesnt matter **keep this in mind moving // forward.
+
+var flightNum = "Fa5398"; // output is Fa5398
+var flightNum = "Fa5398"; // output is Fa5398
+
+var quote0 = "He's fine"; // single quote in double quotes
+var quote1 = "She's named 'Jessica'."; // single quote in double quote
+var quote2 = 'She is named "Jessica".'; // double quotes in single
+var quote3 = "She's named 'Jessica'."; // using the backslash you can break in and insert single quotes in single quotes
+
+console.log(quote0);
+console.log("------------------");
+console.log(quote1);
+console.log("------------------");
+console.log(quote2);
+console.log("------------------");
+console.log(quote3);
+console.log("------------------");
+
+// Write your own strings below for practice. console.log() your string variables
+// type node strings.js in the terminal and see what happens!
